7a01ba931a3f7d84e3835b6a6b040992350c6a17
Nabin-R <72493874+mdnabinraj@users.noreply.github.com>
2024-02-28 22:50:28 +0000

    Update

--- DAY38 ASSIGNMENT/Assignment.sql
+++ DAY38 ASSIGNMENT/Assignment.sql
@@ -1,3 +1,4 @@
+-- By Nabin
 create database if not exists assignment;
 use assignment;
  -- Creating Tables
@@ -114,13 +115,56 @@ from Breakdowns LEFT JOIN Vehicles ON Breakdowns.VehicleReg=Vehicles.VehicleReg 
 select VehicleReg, count(VehicleReg) as '# of breakdowns' from Breakdowns group by VehicleReg having count(VehicleReg) > 1; 
 
 
--- ----------------- Task 3 ------------------- --
+-- ----------------- Task 4 ------------------- --
 
+-- All the vehicles a member owns. For example, Matt 
+select Members.MemberID, concat(MemberFName, ' ', MemberLName) as 'Member', group_concat(' ',VehicleMake, '-', VehicleModel) as 'Vehicle Owned' 
+from Members left join Vehicles on Vehicles.MemberID=Members.MemberID group by Members.MemberID ; -- All Member List
 
+select Members.MemberID, concat(MemberFName, ' ', MemberLName) as 'Member', group_concat(' ',VehicleMake, '-', VehicleModel) as 'Vehicle Owned' 
+from Members left join Vehicles on Vehicles.MemberID=Members.MemberID where Members.MemberID = 123456789 group by Members.MemberID ; -- Specific Member
 
+-- The number of vehicles each member has – sort the data based on the number of cars	 from highest to lowest. 
+select Members.MemberID, concat(MemberFName, ' ', MemberLName) as 'Member', count(Vehicles.MemberID) as '# of Cars' 
+from Members left join Vehicles on Vehicles.MemberID=Members.MemberID group by Members.MemberID order by count(Vehicles.MemberID) DESC ;
 
+-- All vehicles that have broken down in a particular location along with member details. 
+select concat(Vehicles.VehicleMake, '-', Vehicles.VehicleModel) as 'Car', concat(MemberFName, ' ', MemberLName) as 'Car Owner', Breakdowns.BreakdownLoc 
+from Breakdowns left join Vehicles on Breakdowns.VehicleReg = Vehicles.VehicleReg left join Members on Members.MemberID = Vehicles.MemberID; -- All breakdown location
 
+select concat(Vehicles.VehicleMake, '-', Vehicles.VehicleModel) as 'Car', concat(MemberFName, ' ', MemberLName) as 'Car Owner', Breakdowns.BreakdownLoc 
+from Breakdowns left join Vehicles on Breakdowns.VehicleReg = Vehicles.VehicleReg left join Members on Members.MemberID = Vehicles.MemberID where Members.MemberID = 333445555; -- Specific breakdown location
 
+-- A list of all breakdowns along with member and engineer details between two dates. 
+select concat(MemberFName, ' ', MemberLName) as 'Member', concat(EngineerFName, ' ', EngineerLName) as 'Engineer', Breakdowns.BreakdownDate, BreakdownTime 
+from Breakdowns left join Engineers on Breakdowns.EngineerID = Engineers.EngineerID left join Vehicles on Breakdowns.VehicleReg = Vehicles.VehicleReg Left Join Members on Members.MemberID = Vehicles.MemberID where Breakdowns.BreakdownDate between '01-02-11' and '01-05-01'
+order by Breakdowns.BreakdownDate;
+
+-- A further 3 relational queries of your choice that are meaningful to the company. 
+
+-- List Car & Date of specific engineer works
+select concat(EngineerFName, ' ', EngineerLName) as 'Engineer', concat(Vehicles.VehicleMake, '-', Vehicles.VehicleModel) as 'Car',  BreakdownDate 
+from Breakdowns left join Engineers on Breakdowns.EngineerID = Engineers.EngineerID left join Vehicles on Breakdowns.VehicleReg = Vehicles.VehicleReg where Engineers.EngineerID = 88291011;
+
+-- Count amount of work each engineer does
+select concat(EngineerFName, ' ', EngineerLName) as 'Engineer', count(breakdowns.breakdownID) as 'Total Work'
+from Breakdowns left join Engineers on Breakdowns.EngineerID = Engineers.EngineerID group by breakdowns.EngineerID;
+
+-- Member Detail, Car Detail and Date for maintenance for specific Member
+select concat(MemberFName, ' ', MemberLName) as 'Member', concat(Vehicles.VehicleMake, '-', Vehicles.VehicleModel) as 'Car', BreakdownDate 
+from Breakdowns left join Vehicles on Breakdowns.VehicleReg = Vehicles.VehicleReg left join Members on Vehicles.MemberID = Members.MemberID where Members.MemberID = 123456789;
+
+/*Task 5 
+
+Using W3Schools or any other resource research the following functions – Avg, Max, Min, Sum.  Explain with examples how each one is used.  
+
+Task 6 
+
+If a member has more than one vehicle, then display multi-car policy 
+
+Create a stored procedure which will display number of cars for any member whose first name and last name you are passing as argument while calling the stored procedure! 
+
+*/
 
 
 
