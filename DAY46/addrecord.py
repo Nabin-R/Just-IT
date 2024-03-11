748c9a79de208296bbe0001190fca9287b6ee005
Nabin-R <72493874+mdnabinraj@users.noreply.github.com>
2024-03-11 16:59:14 +0000

    day46

---
+++ DAY46/addrecord.py
@@ -0,0 +1,44 @@
+"Objectives"
+"" '' # Import connect module
+"" '' # Create a function to add record(s) to a table in a database
+"" '' # Use try and except to handle error(s)
+"" '' # Use the execute method from the cursor object to run sql statement
+
+
+"" '' # Notes
+"" '' # The SQL statement may be parametrized (i. e. placeholders instead of SQL literals). 
+"" '' # A parameter specifies the value a particular field must contain when carrying out a query. 	
+from connect import *
+ 
+"" '' # Create a function to add record(s) to a table in a database
+"" '' # Use try and except to handle error(s)
+"" '' # Use the execute method from the cursor object to run sql statement
+ 
+ 
+"" '' # Notes
+"" '' # The SQL statement may be parametrized (i. e. placeholders instead of SQL literals).
+"" '' # A parameter specifies the value a particular field must contain when carrying out a query.  
+ 
+# ceate a function to insert ecord in the songs table
+def insert_reord():
+    try:
+        dbCon, dbCursor = db_access()
+        # fields/columns = SongID, Title, Artist, Genre
+        # SongID field is auto increment, no data required
+ 
+        # create variables to store the input for the respective fields
+        song_title = input("Enter song title: ")
+        song_artist = input("Enter song artist: ")
+        song_genre = input("Enter song genre: ")
+ 
+       
+        # Create a sql insert statement to inset data from the rspective variables above
+        dbCursor.execute("INSERT INTO songs (Title,Artist,Genre) VALUES(?,?,?)", (song_title, song_artist, song_genre))
+        # Call/invoke the commit method to save the changes(record) permanently in the db table
+        dbCon.commit()
+ 
+        print(f"{song_title}  inserted in the songs table")
+    except sql.OperationalError as oe:
+        print(f"Failed because {oe}")
+if __name__ == "__main__":
+    insert_reord()
---
+++ DAY46/createTbls.py
@@ -0,0 +1,48 @@
+"Objectives"
+"" '' # Import connect module
+"" '' # Create and use a function that create tables in a database
+"" '' # Use try and except to handle error(s)
+"" '' # Use the execute method from the cursor object to run sql statement
+
+from connect import *
+
+def create_tbls():
+    dbCon, dbCursor = db_access()
+    dbCursor.execute(
+    """ 
+    CREATE TABLE "members" (
+	    "MemberID"	INTEGER NOT NULL UNIQUE,
+	    "Firstname"	TEXT,
+	    "Lastname"	TEXT,
+	    "Email"	TEXT,
+	    PRIMARY KEY("MemberID" AUTOINCREMENT)
+    )"""
+    )
+    # ...............................
+    dbCursor.execute(
+    """
+    CREATE TABLE "songs" (
+	    "SongID"	INTEGER NOT NULL UNIQUE,
+	    "Title"	TEXT,
+	    "Artist"	TEXT,
+	    "Genre"	TEXT,
+	    PRIMARY KEY("SongID" AUTOINCREMENT)
+    )"""
+    )
+    # ...........................
+    dbCursor.execute(
+    """
+    CREATE TABLE "downloads" (
+	    "DownlID"	INTEGER NOT NULL UNIQUE,
+	    "SongID"	INTEGER,
+	    "MemberID"	INTEGER,
+	    "Date"	TEXT,
+	    PRIMARY KEY("DownlID" AUTOINCREMENT),
+	    FOREIGN KEY("SongID") REFERENCES "songs"("SongID"),
+	    FOREIGN KEY("MemberID") REFERENCES "members"("MemberID")
+)
+"""
+)
+if __name__ == "__main__":
+    create_tbls()
+
